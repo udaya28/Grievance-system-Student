5c825ca8d37ed221bcc15703095f285dee6cbaae
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-11-30 20:37:11 +0530

    added content

---
+++ src/components/details/Details.component.jsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import './Details.styles.css';
+const Details = () => {
+  return (
+    <div className="details">
+      <h1>Students’ Grievance Redressal Cell</h1>
+      <p>
+        The cell functions to look into the grievances lodged by any student.
+        Students may approach the cell to voice their grievances regarding
+        academic matters, health services, library and other services. Any form
+        of discontent or dissatisfaction on the part of the students can be
+        informed to the cell. The Grievance cell also looks into matters of
+        harassment. Anyone with a genuine grievance may approach the
+        Co-ordinator or member of the Students’ Grievance cell. Grievances may
+        also be sent through e-mail to the Co-ordinator of the Students’
+        Grievance Cell. The cell then redresses the grievances promptly and
+        judiciously. As a result of this mechanism, the institute has pleasant
+        ambient atmosphere and good work culture with in-built goodwill and
+        mutual understanding among the students.
+      </p>
+      <h1>Objective</h1>
+      <p>
+        The main objective of the Grievance Cell is to promote and maintain a
+        conducive and harmonious educational environment among the students. The
+        Students’ Grievance Cell is constituted for the redressal of the
+        problems reported by the students of the College. This is with the
+        following objectives in mind
+      </p>
+
+      <ul>
+        Grievance cell is formed in order to keep the healthy working atmosphere
+        amongst staff, students and parents. This cell helps Students to record
+        their complaints and solve their problems related to academics,
+        resources and personal grievancesfreely and franklywithout any fear of
+        victimization.
+      </ul>
+      <ul>
+        To keep the dignity of the college high by ensuring conflict free
+        atmosphere in the College by promoting good Student-Student relationship
+        and Student-teacher relationship.
+      </ul>
+      <ul>
+        To ensure effective solution to the student grievances with an impartial
+        and fair approach
+      </ul>
+      <ul>
+        In order to advising Students of the College to respect each other and
+        be patient whenever any occasion of conflict arises.
+      </ul>
+      <ul>
+        To advise all the students to refrain from stirring up students against
+        other students, teachers and College administration.
+      </ul>
+      <ul>
+        Ragging in any form is strictly prohibited in and outside the
+        institution. Any violation of ragging and disciplinary rules should be
+        urgently brought to the notice of the Principal.Ragging Complaints will
+        be handled as per ragging rules.
+      </ul>
+      <ul>
+        Woman Harassment complaints will be handled as per government guidelines
+        by respective section.
+      </ul>
+      <h1>Scope</h1>
+      <p>
+        The cell will deal with Grievances received in writing from the students
+        about any of the following matters:
+      </p>
+      <ul>
+        Academic Matters: Related to timely issue of duplicate Mark-sheets,
+        Transfer Certificates, Conduct Certificates or other examination related
+        matters.
+      </ul>
+      <ul>
+        Financial matters: Related to dues and payments for various items from
+        library, hostels etc.
+      </ul>
+      <ul>
+        Other Matters: Related to certain misgivings about conditions of
+        sanitation, preparation of food, availability of transport,
+        victimization by teachers etc.
+      </ul>
+      <h1>Functions</h1>
+      <ul>
+        {' '}
+        Complaint Boxes have been installed in the College campus in which the
+        Students, who want to remain anonymous, canput in writing their
+        grievances and their suggestions for improving the
+        academics/administration in the College. Students can lodge a complaint.
+      </ul>
+      <ul>
+        The person concerned can personally approach to any member of the Cell
+        and can send email or write an application and submit to Cell Convener
+        for grievances of any sort.
+      </ul>
+      <ul>
+        The cases will be attended promptly on receipt of grievances from the
+        students.
+      </ul>
+      <ul>
+        The cell will review all cases and will act formally accordingly as per
+        the Management policy.
+      </ul>
+      <ul>
+        The cell will give report to the authority about the cases attended to
+        and the number of pending cases, if any, which require direction and
+        guidance from the higher authorities.
+      </ul>
+      <h1>Exclusions</h1>
+      <p>
+        The grievance Redressal cell shall not entertain the following issues:
+      </p>
+
+      <ul>
+        Decisions of the executive council, academic council, board of studies
+        and other administrative or academic committees constituted by the
+        university.
+      </ul>
+      <ul>
+        Decisions with regard to award of scholarship, fee concessions, medals
+        etc.
+      </ul>
+      <ul>
+        Decisions made by the university with regard to disciplinary matters and
+        misconduct.
+      </ul>
+      <ul>
+        Decisions of the university about admissions in any courses offered by
+        the institute.
+      </ul>
+      <ul>
+        Decisions by competent authority on assessment and examination result.
+      </ul>
+    </div>
+  );
+};
+
+export default Details;
--- src/components/home/Home.component.jsx
+++ src/components/home/Home.component.jsx
@@ -6,6 +6,7 @@ import Paper from '@material-ui/core/Paper';
 import Header from './../header/Header.component';
 import './Home.styles.css';
 import ImageHeader from '../imageHeader/ImageHeader';
+import Details from './../details/Details.component';
 const Home = () => {
   return (
     <Paper>
@@ -13,43 +14,7 @@ const Home = () => {
       <ImageHeader />
       <Container maxWidth="md">
         <Grid container direction="column">
-          <Box className="box" >
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Cupiditate voluptates minima ea error sunt nam fuga consectetur?
-              Inventore, error totam velit ipsam quos omnis ullam a non est.
-              Ducimus, eius! Lorem ipsum, dolor sit amet consectetur adipisicing
-              elit. Doloribus dignissimos dolor facilis vero, et consequatur
-              inventore ducimus natus tempora eius cumque, neque sapiente!
-              Aperiam delectus tenetur repellendus cupiditate a vitae? Lorem
-              ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
-              voluptates minima ea error sunt nam fuga consectetur? Inventore,
-              error totam velit ipsam quos omnis ullam a non est. Ducimus, eius!
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Doloribus dignissimos dolor facilis vero, et consequatur inventore
-              ducimus natus tempora eius cumque, neque sapiente! Aperiam
-              delectus tenetur repellendus cupiditate a vitae?Lorem ipsum, dolor
-              sit amet consectetur adipisicing elit. Cupiditate voluptates
-              minima ea error sunt nam fuga consectetur? Inventore, error totam
-              velit ipsam quos omnis ullam a non est. Ducimus, eius! Lorem
-              ipsum, dolor sit amet consectetur adipisicing elit. Doloribus
-              dignissimos dolor facilis vero, et consequatur inventore ducimus
-              natus tempora eius cumque, neque sapiente! Aperiam delectus
-              tenetur repellendus cupiditate a vitae?
-            </p>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
-              corrupti impedit eaque fugit placeat, perferendis cumque
-              veritatis, est blanditiis iusto ipsam nam porro! Numquam, ab autem
-              neque ratione voluptatum assumenda?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero
-              nisi corporis qui ea! Impedit sequi autem voluptatibus iure
-              debitis soluta iste, nemo quis similique aut velit veniam
-              praesentium vitae expedita.
-            </p>
-          </Box>
+          <Details />
         </Grid>
       </Container>
     </Paper>
