851ee0306e835a7ff4d140a0e9dc9b11bab7a007
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-11-30 21:18:12 +0530

    added styles to li

--- src/components/details/Details.component.jsx
+++ src/components/details/Details.component.jsx
@@ -26,110 +26,119 @@ const Details = () => {
         problems reported by the students of the College. This is with the
         following objectives in mind
       </p>
+      <ul>
+        <li>
+          Grievance cell is formed in order to keep the healthy working
+          atmosphere amongst staff, students and parents. This cell helps
+          Students to record their complaints and solve their problems related
+          to academics, resources and personal grievancesfreely and
+          franklywithout any fear of victimization.
+        </li>
+        <li>
+          To keep the dignity of the college high by ensuring conflict free
+          atmosphere in the College by promoting good Student-Student
+          relationship and Student-teacher relationship.
+        </li>
+        <li>
+          To ensure effective solution to the student grievances with an
+          impartial and fair approach
+        </li>
+        <li>
+          In order to advising Students of the College to respect each other and
+          be patient whenever any occasion of conflict arises.
+        </li>
+        <li>
+          To advise all the students to refrain from stirring up students
+          against other students, teachers and College administration.
+        </li>
+        <li>
+          Ragging in any form is strictly prohibited in and outside the
+          institution. Any violation of ragging and disciplinary rules should be
+          urgently brought to the notice of the Principal.Ragging Complaints
+          will be handled as per ragging rules.
+        </li>
+        <li>
+          Woman Harassment complaints will be handled as per government
+          guidelines by respective section.
+        </li>
+      </ul>
 
-      <ul>
-        Grievance cell is formed in order to keep the healthy working atmosphere
-        amongst staff, students and parents. This cell helps Students to record
-        their complaints and solve their problems related to academics,
-        resources and personal grievancesfreely and franklywithout any fear of
-        victimization.
-      </ul>
-      <ul>
-        To keep the dignity of the college high by ensuring conflict free
-        atmosphere in the College by promoting good Student-Student relationship
-        and Student-teacher relationship.
-      </ul>
-      <ul>
-        To ensure effective solution to the student grievances with an impartial
-        and fair approach
-      </ul>
-      <ul>
-        In order to advising Students of the College to respect each other and
-        be patient whenever any occasion of conflict arises.
-      </ul>
-      <ul>
-        To advise all the students to refrain from stirring up students against
-        other students, teachers and College administration.
-      </ul>
-      <ul>
-        Ragging in any form is strictly prohibited in and outside the
-        institution. Any violation of ragging and disciplinary rules should be
-        urgently brought to the notice of the Principal.Ragging Complaints will
-        be handled as per ragging rules.
-      </ul>
-      <ul>
-        Woman Harassment complaints will be handled as per government guidelines
-        by respective section.
-      </ul>
       <h1>Scope</h1>
       <p>
         The cell will deal with Grievances received in writing from the students
         about any of the following matters:
       </p>
       <ul>
-        Academic Matters: Related to timely issue of duplicate Mark-sheets,
-        Transfer Certificates, Conduct Certificates or other examination related
-        matters.
+        <li>
+          Academic Matters: Related to timely issue of duplicate Mark-sheets,
+          Transfer Certificates, Conduct Certificates or other examination
+          related matters.
+        </li>
+        <li>
+          Financial matters: Related to dues and payments for various items from
+          library, hostels etc.
+        </li>
+        <li>
+          Other Matters: Related to certain misgivings about conditions of
+          sanitation, preparation of food, availability of transport,
+          victimization by teachers etc.
+        </li>
       </ul>
-      <ul>
-        Financial matters: Related to dues and payments for various items from
-        library, hostels etc.
-      </ul>
-      <ul>
-        Other Matters: Related to certain misgivings about conditions of
-        sanitation, preparation of food, availability of transport,
-        victimization by teachers etc.
-      </ul>
+
       <h1>Functions</h1>
       <ul>
-        {' '}
-        Complaint Boxes have been installed in the College campus in which the
-        Students, who want to remain anonymous, canput in writing their
-        grievances and their suggestions for improving the
-        academics/administration in the College. Students can lodge a complaint.
+        <li>
+          Complaint Boxes have been installed in the College campus in which the
+          Students, who want to remain anonymous, canput in writing their
+          grievances and their suggestions for improving the
+          academics/administration in the College. Students can lodge a
+          complaint.
+        </li>
+        <li>
+          The person concerned can personally approach to any member of the Cell
+          and can send email or write an application and submit to Cell Convener
+          for grievances of any sort.
+        </li>
+        <li>
+          The cases will be attended promptly on receipt of grievances from the
+          students.
+        </li>
+        <li>
+          The cell will review all cases and will act formally accordingly as
+          per the Management policy.
+        </li>
+        <li>
+          The cell will give report to the authority about the cases attended to
+          and the number of pending cases, if any, which require direction and
+          guidance from the higher authorities.
+        </li>
       </ul>
-      <ul>
-        The person concerned can personally approach to any member of the Cell
-        and can send email or write an application and submit to Cell Convener
-        for grievances of any sort.
-      </ul>
-      <ul>
-        The cases will be attended promptly on receipt of grievances from the
-        students.
-      </ul>
-      <ul>
-        The cell will review all cases and will act formally accordingly as per
-        the Management policy.
-      </ul>
-      <ul>
-        The cell will give report to the authority about the cases attended to
-        and the number of pending cases, if any, which require direction and
-        guidance from the higher authorities.
-      </ul>
+
       <h1>Exclusions</h1>
       <p>
         The grievance Redressal cell shall not entertain the following issues:
       </p>
-
       <ul>
-        Decisions of the executive council, academic council, board of studies
-        and other administrative or academic committees constituted by the
-        university.
-      </ul>
-      <ul>
-        Decisions with regard to award of scholarship, fee concessions, medals
-        etc.
-      </ul>
-      <ul>
-        Decisions made by the university with regard to disciplinary matters and
-        misconduct.
-      </ul>
-      <ul>
-        Decisions of the university about admissions in any courses offered by
-        the institute.
-      </ul>
-      <ul>
-        Decisions by competent authority on assessment and examination result.
+        <li>
+          Decisions of the executive council, academic council, board of studies
+          and other administrative or academic committees constituted by the
+          university.
+        </li>
+        <li>
+          Decisions with regard to award of scholarship, fee concessions, medals
+          etc.
+        </li>
+        <li>
+          Decisions made by the university with regard to disciplinary matters
+          and misconduct.
+        </li>
+        <li>
+          Decisions of the university about admissions in any courses offered by
+          the institute.
+        </li>
+        <li>
+          Decisions by competent authority on assessment and examination result.
+        </li>
       </ul>
     </div>
   );
