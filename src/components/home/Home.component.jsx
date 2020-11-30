100c6c0745fe278339df7b2f16afec9563147f1b
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-11-30 16:53:10 +0530

    added font

--- src/components/home/Home.component.jsx
+++ src/components/home/Home.component.jsx
@@ -1,30 +1,54 @@
 import React from 'react';
-import Box from '@material-ui/core/Box'
-import Container from '@material-ui/core/Container'
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Header from './../header/Header.component';
 import './Home.styles.css';
 import ImageHeader from '../imageHeader/ImageHeader';
 const Home = () => {
   return (
     <Paper>
+      <Header />
+      <ImageHeader />
       <Container maxWidth="md">
         <Grid container direction="column">
-          <Header />
-          <ImageHeader/>
           <Box className="box">
-            <Paper>
-              <Typography>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Cupiditate voluptates minima ea error sunt nam fuga consectetur?
-                Inventore, error totam velit ipsam quos omnis ullam a non est.
-                Ducimus, eius!
-              </Typography>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero corrupti impedit eaque fugit placeat, perferendis cumque veritatis, est blanditiis iusto ipsam nam porro! Numquam, ab autem neque ratione voluptatum assumenda?</p>
-            </Paper>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero nisi corporis qui ea! Impedit sequi autem voluptatibus iure debitis soluta iste, nemo quis similique aut velit veniam praesentium vitae expedita.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Cupiditate voluptates minima ea error sunt nam fuga consectetur?
+              Inventore, error totam velit ipsam quos omnis ullam a non est.
+              Ducimus, eius! Lorem ipsum, dolor sit amet consectetur adipisicing
+              elit. Doloribus dignissimos dolor facilis vero, et consequatur
+              inventore ducimus natus tempora eius cumque, neque sapiente!
+              Aperiam delectus tenetur repellendus cupiditate a vitae? Lorem
+              ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
+              voluptates minima ea error sunt nam fuga consectetur? Inventore,
+              error totam velit ipsam quos omnis ullam a non est. Ducimus, eius!
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Doloribus dignissimos dolor facilis vero, et consequatur inventore
+              ducimus natus tempora eius cumque, neque sapiente! Aperiam
+              delectus tenetur repellendus cupiditate a vitae?Lorem ipsum, dolor
+              sit amet consectetur adipisicing elit. Cupiditate voluptates
+              minima ea error sunt nam fuga consectetur? Inventore, error totam
+              velit ipsam quos omnis ullam a non est. Ducimus, eius! Lorem
+              ipsum, dolor sit amet consectetur adipisicing elit. Doloribus
+              dignissimos dolor facilis vero, et consequatur inventore ducimus
+              natus tempora eius cumque, neque sapiente! Aperiam delectus
+              tenetur repellendus cupiditate a vitae?
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+              corrupti impedit eaque fugit placeat, perferendis cumque
+              veritatis, est blanditiis iusto ipsam nam porro! Numquam, ab autem
+              neque ratione voluptatum assumenda?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero
+              nisi corporis qui ea! Impedit sequi autem voluptatibus iure
+              debitis soluta iste, nemo quis similique aut velit veniam
+              praesentium vitae expedita.
+            </p>
           </Box>
         </Grid>
       </Container>
