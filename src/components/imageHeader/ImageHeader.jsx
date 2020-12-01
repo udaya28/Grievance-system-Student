db601e01b37f4c84c5f5a250d674398c6f1e8e56
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-12-01 11:48:55 +0530

    changed image

--- src/components/imageHeader/ImageHeader.jsx
+++ src/components/imageHeader/ImageHeader.jsx
@@ -3,8 +3,8 @@ import './ImageHeader.css'
 
 const ImageHeader = () => (
     <div className="image-container">
-        <h1 style={{fontSize:"30px"}}>STUDENTS' GRIEVANCE CELL</h1>
-        <h2 style={{fontSize:"20px"}}>VCET</h2>
+        <h1 className="head" style={{fontSize:"30px"}}>STUDENTS<span> GRIEVANCE CELL</span></h1>
+        <h1 style={{fontSize:"20px"}}>&lt; VCET &#47;&gt;</h1>
     </div>
     
 );
