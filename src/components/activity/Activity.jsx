67c282b323cf25a23203a9563c7f15b1b613ecc2
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-12-04 20:47:08 +0530

    completed Recent activity basic

--- src/components/activity/Activity.jsx
+++ src/components/activity/Activity.jsx
@@ -1,22 +1,22 @@
-import { Card, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import React from 'react';
 import './Activity.css';
 
 const sample = [
   {
-    title: 'complaint about online class',
+    title: 'Complaint about online class',
     timeStamp: "Fri Dec 04 2020 19:21:33 GMT+0530 (India Standard Time)",
-    status: 'seen', //  "unseen" | "replayed",
-    category: 'college', // "hostel"
+    status: 'unseen', //  "unseen" | "replayed",
+    category: 'hostel', // "hostel"
     complaint:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis similique debitis distinctio nobis. Perferendis voluptate, obcaecati deleniti nostrum impedit doloribus quod soluta. Eos et corporis consequuntur id ipsa impedit?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis similique debitis distinctio nobis. Perferendis voluptate, obcaecati deleniti nostrum impedit doloribus quod soluta. Eos et corporis consequuntur id ipsa impedit?',
     response:
       ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci dolorum blanditiis odio, iste veniam, iusto nemo veritatis quas aliquam quibusdam accusantium! Aut hic earum distinctio nostrum autem cumque praesentium repellendus!',
   },
   {
-    title: 'complaint about online class',
-    timeStamp: Date(),
-    status: 'seen', //  "unseen" | "replayed",
+    title: 'Need information about Bus transport',
+    timeStamp: "Fri Dec 04 2020 20:09:31 GMT+0530 (India Standard Time)",
+    status: 'unseen', //  "unseen" | "replayed",
     category: 'college', // "hostel"
     complaint:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis similique debitis distinctio nobis. Perferendis voluptate, obcaecati deleniti nostrum impedit doloribus quod soluta. Eos et corporis consequuntur id ipsa impedit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis similique debitis distinctio nobis. Perferendis voluptate, obcaecati deleniti nostrum impedit doloribus quod soluta. Eos et corporis consequuntur id ipsa impedit?',
@@ -24,19 +24,19 @@ const sample = [
       ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci dolorum blanditiis odio, iste veniam, iusto nemo veritatis quas aliquam quibusdam accusantium! Aut hic earum distinctio nostrum autem cumque praesentium repellendus!',
   },
   {
-    title: 'complaint about online class',
-    timeStamp: Date(),
+    title: 'Complaint about hostel food',
+    timeStamp: "Fri Dec 04 2020 22:45:31 GMT+0530 (India Standard Time)",
     status: 'seen', //  "unseen" | "replayed",
-    category: 'college', // "hostel"
+    category: 'hostel', // "hostel"
     complaint:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis similique debitis distinctio nobis. Perferendis voluptate, obcaecati deleniti nostrum impedit doloribus quod soluta. Eos et corporis consequuntur id ipsa impedit?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis similique debitis distinctio nobis. Perferendis voluptate, obcaecati deleniti nostrum impedit doloribus quod soluta. Eos et corporis consequuntur id ipsa impedit?',
     response:
       ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci dolorum blanditiis odio, iste veniam, iusto nemo veritatis quas aliquam quibusdam accusantium! Aut hic earum distinctio nostrum autem cumque praesentium repellendus!',
   },
   {
-    title: 'complaint about online class',
-    timeStamp: Date(),
-    status: 'seen', //  "unseen" | "replayed",
+    title: 'About online exam portal',
+    timeStamp: "Fri Dec 04 2020 02:45:31 GMT+0530 (India Standard Time)",
+    status: 'replayed', //  "unseen" | "replayed",
     category: 'college', // "hostel"
     complaint:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis similique debitis distinctio nobis. Perferendis voluptate, obcaecati deleniti nostrum impedit doloribus quod soluta. Eos et corporis consequuntur id ipsa impedit?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis similique debitis distinctio nobis. Perferendis voluptate, obcaecati deleniti nostrum impedit doloribus quod soluta. Eos et corporis consequuntur id ipsa impedit?',
@@ -47,17 +47,35 @@ const sample = [
 
 const getDate = (date) => {
   let arr = date.split(' ').slice(1, 5);
-
-  let time = arr[3].split(":").map((str)=>parseInt(str))
-  console.log(arr,time)
-  return date.split(' ').slice(1, 5).join(' ');
+  let timeArr = arr[3].split(":").map((str)=>parseInt(str))
+  let AMPMstr = "AM";
+  let hour = timeArr[0] 
+  let min = timeArr[1]
+  let day = arr.slice(0,3)
+  let dayStr = day.join(" ")
+  if(timeArr[0] >= 12){
+    AMPMstr = "PM";
+    hour %=12;
+  }
+  let time = `${hour>9?hour : "0"+hour }:${min>9?min:"0"+min} ${AMPMstr}`
+  return `${dayStr +" "+ time}`;
 };
 
 const ActivityCard = ({ data }) => {
   const { title, timeStamp, status, complaint, response, category } = data;
+  let statusClass ;
+  if(status === "seen"){
+      statusClass = "yellow"
+  }else if(status === "unseen"){
+      statusClass = "red"
+  }else if(status === "replayed"){
+      statusClass = "green"
+  }else{
+    statusClass = ""
+  }
   return (
     <Grid item xs={12} sm={6}>
-      <Grid container className="activity-container">
+      <Grid container className={`activity-container + ${statusClass}`}>
         <Grid item>
           <h1 className="activity-title">{title}</h1>
         </Grid>
